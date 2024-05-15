212c90c5a734bf803c5f905b9cfef4d1d98b987a
Vishal Jaiswal <jvishal8794@gmail.com>
2024-05-15 22:46:21 +0530

    svg changes on header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import './Header.css'
 
+import CartSvg from '../../assets/SVG/cart.svg'
+
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" sx={{bgcolor: "#ffffff", color: "black"}}>
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ display: 'flex', flexGrow: 1 }}>
+               <p className='header-title'> DzuoKart</p> 
+               <img className='svg-cart' src={CartSvg} alt="" />
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
 }
 
 export default Header
