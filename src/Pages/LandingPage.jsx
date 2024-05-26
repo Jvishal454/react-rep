e0b93f274b1d0cf0ce410042647f6de21b081710
Vishal Jaiswal <jvishal8794@gmail.com>
2024-05-26 13:43:33 +0530

    added ui for multiple components

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from '../Components/Home/Home'
+import ExploreMenu from '../Components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../Components/FoodDisplay/FoodDisplay';
+import './LandingPage.css'
 
 const LandingPage = () => {
+
+  const [category, setCategory] = useState("All");
   return (
     <div>
         <Home />
+        <ExploreMenu category={category} setCategory={setCategory} className="explore-menu" />
+        <FoodDisplay  category={category} className="food-display" />
     </div>
   )
 }
